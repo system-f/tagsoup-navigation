568614abe9db8e2747002f35dcc15d89ef51dae4
Tony Morris <tmorris@tmorris.net>
2018-11-10 17:14:29 +1000

    .

--- src/Text/HTML/TagSoup/Navigation.hs
+++ src/Text/HTML/TagSoup/Navigation.hs
@@ -1,32 +1,8 @@
 {-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE FunctionalDependencies #-}
 
-module Text.HTML.TagSoup.Navigation where
+module Text.HTML.TagSoup.Navigation(
+  module N
+) where
 
-
-{-
-
-data TagTree str
-  = TagBranch str
-              [Attribute str]
-              [TagTree str]
-  | TagLeaf (Tag str)
-
-
-type Attribute str = (str, str)
-
-data Tag str
-  = TagOpen str [Attribute str]
-  | TagClose str
-  | TagText str
-  | TagComment str
-  | TagWarning str
-  | TagPosition Row Column
-
-type Row = Int
-type Column = Int
-
--}
+import Text.HTML.TagSoup.Navigation.Parse as N
+import Text.HTML.TagSoup.Navigation.Types as N
---
+++ src/Text/HTML/TagSoup/Navigation/Parse.hs
@@ -0,0 +1,7 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Text.HTML.TagSoup.Navigation.Parse(
+  module P
+) where
+
+import Text.HTML.TagSoup.Navigation.Parse.ParseOptions as P
